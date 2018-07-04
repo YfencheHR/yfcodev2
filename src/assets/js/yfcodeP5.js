56bdcbb1a4f89305c6e147b48a85858d017ebe28
Yfenche Hernandez <yfenchehr@gmail.com>
2018-07-04 23:54:48 +0100

    Primer refactor de codigo de la escena Incluida interactividad con las nubes

--- src/assets/js/yfcodeP5.js
+++ src/assets/js/yfcodeP5.js
@@ -1,34 +1,42 @@
-
-// Demonstración de múltiples fuerzas acutando en
-// cuerpos (con clase Mover)
-// cuerpos sujetos continuamente a gravedad
-// cuerpos sujetos a resistencia de fluidos cuando están en el "agua"
-
 // Cinco cuerpos en movimiento
-var movers = [];
-var nubes = [];
+let cajas;
+let nubes;
+let snowflakes;
+// Entorno
+let logo;
+let nube;
+let liquid;
 
-let snowflakes = [];
-// Líquido
+let widthP;
+let heightP;
 
-var widthP;
-var heightP;
-var imagenes =  {
+let frameCount = 0;
+let segLength = 50;
+
+let config = {
+  cajas: {
+    total: 5,
+    fondo: 0
+  },
+  nubes: {
+    total: 1
+  },
+  nieve: {
+    total: 250
+  }
+};
+let imagenes =  {
   logo: "assets/img/logo.png",
   nube: "assets/img/nube2.png",
   luna: "assets/img/nube2.png"
 };
-var logo;
-var nube;
-var liquid;
-var totalIdeas = 5;
 
-var controlEscena = 'inicio';
+let controlEscena = 'inicio';
 
 function setup() {
   widthP = document.getElementById('sketch-holder').offsetWidth;
   heightP = document.getElementById('sketch-holder').offsetHeight;
-  var canvas = createCanvas(widthP, heightP);
+  let canvas = createCanvas(widthP, heightP);
   reset();
   canvas.parent('sketch-holder');
   // Crear objeto líquido
@@ -38,28 +46,34 @@ function setup() {
 }
 
 function draw() {
+  // mostrar entorno
+  entorno();
+
+  if (controlEscena === 'inicio') {
+    animarCajas();
+  } else if (controlEscena === 'escuchando'){
+
+  }
+  animarNubes();
+  nieve();
+}
+
+function entorno() {
+  // fondo
   background(200);
   // Dibujar el agua
   liquid.display();
-
-  for (var i = 0; i < totalIdeas; i++) {
-
-    if (controlEscena === 'inicio') {
-      escenaIncio(i);
-    }
-
-  }
-  nieve();
-
-
 }
 
 function nieve() {
+
   let t = frameCount / 60; // update time
 
   // create a random number of snowflakes each frame
-  for (var i = 0; i < random(5); i++) {
-    snowflakes.push(new snowflake()); // append snowflake object
+  if (snowflakes.length < config.nieve.total) {
+    for (let i = 0; i < random(2); i++) {
+      snowflakes.push(new Snowflake()); // append snowflake object
+    }
   }
 
   // loop through snowflakes with a for..of loop
@@ -67,34 +81,46 @@ function nieve() {
     flake.update(t); // update snowflake position
     flake.display(); // draw snowflake
   }
+
+  frameCount++;
 }
 
+function animarNubes() {
 
-
-function escenaIncio(i) {
-  // ¿Está el objeto Mover dentro del objeto líquido?
-  if (liquid.contains(movers[i])) {
-    // Calcular fuerza de arrastre
-    var dragForce = liquid.calculateDrag(movers[i]);
-    // Aplicar fuerza de arrastre a Mover
-    movers[i].applyForce(dragForce);
+  for (let nube of nubes) {
+    if (nube.checkMouse()) {
+      nube.move();
+    }
+    nube.display();
   }
+}
 
-  // Aquí se escala la gravedad según la masa
-  var gravity = createVector(0, 0.1*movers[i].mass);
-  // Aplicar gravedad
-  movers[i].applyForce(gravity);
+function animarCajas(i) {
 
-  // Refrescar y mostrar
-  if (movers[i].checkRebote()) {
-    movers[i].updateDown();
-    movers[i].checkEdges();
-  } else {
-    movers[i].updateUp()
+  for (let caja of cajas) {
+    // ¿Está el objeto Mover dentro del objeto líquido?
+    if (liquid.contains(caja)) {
+      // Calcular fuerza de arrastre
+      let dragForce = liquid.calculateDrag(caja);
+      // Aplicar fuerza de arrastre a Mover
+      caja.applyForce(dragForce);
+    }
+
+    // Aquí se escala la gravedad según la masa
+    let gravity = createVector(0, 0.1 * caja.mass);
+    // Aplicar gravedad
+    caja.applyForce(gravity);
+
+    // Refrescar y mostrar
+    if (caja.checkRebote()) {
+      caja.updateDown();
+      caja.checkEdges();
+    } else {
+      caja.updateUp()
+    }
+    caja.display();
+
   }
-  movers[i].display();
-  nubes[i].display();
-
 }
 
 function mousePressed() {
@@ -103,142 +129,174 @@ function mousePressed() {
 
 // Reiniciar todos los objetos Mover aleatoriamente
 function reset() {
-  var multiplicador = widthP / totalIdeas;
-  var m = 0;
-  var x = 0;
-  var y = 0;
-  for (var i = 0; i < totalIdeas; i++) {
+  cajas = [];
+  nubes = [];
+  snowflakes = [];
+  let multiplicador = widthP / config.cajas.total;
+  let m = 0;
+  let x = 0;
+  let y = 0;
+
+  for (let i = 0; i < config.cajas.total; i++) {
     m = random(2.5, 5);
-    x = 40+i*multiplicador;
-    y = random(0, (height/2) - (height/2)/5);
-    movers[i] = new Mover(m, x, y);
-    nubes[i] = new Nube(m + m, x, y);
+    x = 40 + i * multiplicador;
+    y = random(0, (height/2) - (height/2) / 5);
+    cajas.push(new Caja(m, x, y));
+    nubes.push(new Nube(m + m, x, y));
   }
 }
 
-var Nube = function(m, x, y) {
+function Nube(m, x, y) {
   this.mass = m;
   this.x = x;
   this.y = y;
-};
+  this.volumen = this.mass *16;
+  this.mitadVolumen = this.volumen /2;
+  this.distancia = 0;
 
-Nube.prototype.display = function() {
-  /*stroke(0);
-  strokeWeight(2);
-  fill(255,127);*/
-  image(nube, this.x,this.y,this.mass*16,this.mass*16);
-  //ellipse(this.position.x,this.position.y,this.mass*16,this.mass*16);
-};
+  this.speed = {
+    ymin: -1,
+    xmin: -1,
+    ymax: 1,
+    xmax: 1,
+  };
+  this.move = function() {
 
-var Liquid = function(x, y, w, h, c) {
+    if (mouseX > this.x+ this.mitadVolumen) {
+      this.speed.xmin = -1 - (this.distancia/20);
+      this.speed.xmax = 0;
+    } else {
+      this.speed.xmin = 0;
+      this.speed.xmax = 1 + (this.distancia/20);;
+    }
+    if (mouseY > this.y + this.mitadVolumen) {
+      this.speed.ymin = -1 - (this.distancia/20);;
+      this.speed.ymax = 0;
+    } else {
+      this.speed.ymin = 0;
+      this.speed.ymax = 1 +  (this.distancia/20);
+    }
+
+    if (this.x > 0) {
+      this.x += random(this.speed.xmin, this.speed.xmax);
+    }
+    if (this.y > 0) {
+      this.y += random(this.speed.ymin, this.speed.ymax);
+    }
+  };
+  this.checkMouse = function() {
+
+    this.distancia = dist(mouseX, mouseY, this.x + this.mitadVolumen, this.y + this.mitadVolumen);
+    if (this.distancia < this.mitadVolumen) {
+      return true;
+    }
+    return false;
+
+  };
+  this.display = function() {
+     image(nube, this.x,this.y,this.volumen,this.volumen);
+  };
+}
+
+function Liquid(x, y, w, h, c) {
   this.x = x;
   this.y = y;
   this.w = w;
   this.h = h;
   this.c = c;
-};
+  // ¿Está el objeto Mover dentro del objeto líquido?
+  this.contains = function(m) {
+    let l = m.position;
+    return l.x > this.x && l.x < this.x + this.w &&
+      l.y > this.y && l.y < this.y + this.h;
+  };
+  // Calcular fuerza de arrastre
+  this.calculateDrag = function(m) {
+    // Magnitud es coeficiente * velocidad al cuadrado
+    let speed = m.velocity.mag();
+    let dragMagnitude = this.c * speed * speed;
 
-// ¿Está el objeto Mover dentro del objeto líquido?
-Liquid.prototype.contains = function(m) {
-  var l = m.position;
-  return l.x > this.x && l.x < this.x + this.w &&
-    l.y > this.y && l.y < this.y + this.h;
-};
+    // Dirección es el inverso de la velocidad
+    let dragForce = m.velocity.copy();
+    dragForce.mult(-1);
 
-// Calcular fuerza de arrastre
-Liquid.prototype.calculateDrag = function(m) {
-  // Magnitud es coeficiente * velocidad al cuadrado
-  var speed = m.velocity.mag();
-  var dragMagnitude = this.c * speed * speed;
+    // Escalar según magnitud
+    // dragForce.setMag(dragMagnitude);
+    dragForce.normalize();
+    dragForce.mult(dragMagnitude);
+    return dragForce;
+  };
 
-  // Dirección es el inverso de la velocidad
-  var dragForce = m.velocity.copy();
-  dragForce.mult(-1);
+  this.display = function() {
+    noStroke();
+    fill(50);
+    rect(this.x, this.y, this.w, this.h);
+  };
+}
 
-  // Escalar según magnitud
-  // dragForce.setMag(dragMagnitude);
-  dragForce.normalize();
-  dragForce.mult(dragMagnitude);
-  return dragForce;
-};
-
-Liquid.prototype.display = function() {
-  noStroke();
-  fill(50);
-  rect(this.x, this.y, this.w, this.h);
-};
-
-function Mover(m,x,y) {
+function Caja(m,x,y) {
   this.mass = m;
   this.position = createVector(x,y);
   this.velocity = createVector(0,0);
   this.acceleration = createVector(0,0);
   this.cotadorRebotes = 0;
   this.maxRebotes = 30;
-}
+  // Segunda ley de Newton: F = M * A
+  // ó A = F / M
+  this.applyForce = function(force) {
+    let f = p5.Vector.div(force,this.mass);
+    this.acceleration.add(f);
+  };
+  this.checkRebote = function() {
+    return this.cotadorRebotes !== this.maxRebotes;
+  };
+  this.updateDown = function() {
+    // La velocidad es cambiada según la aceleración
+    this.velocity.add(this.acceleration);
+    // La posición es cambiada según la velocidad
+    this.position.add(this.velocity);
+    // Borrar aceleración en cada cuadro
+    this.acceleration.mult(0);
+  };
+  this.updateUp = function() {
+  };
+  this.updateDown = function() {
+    // La velocidad es cambiada según la aceleración
+    this.velocity.add(this.acceleration);
+    // La posición es cambiada según la velocidad
+    this.position.add(this.velocity);
+    // Borrar aceleración en cada cuadro
+    this.acceleration.mult(0);
+  };
+  this.display = function() {
+    image(logo, this.position.x,this.position.y,this.mass*16,this.mass*16);
 
-// Segunda ley de Newton: F = M * A
-// ó A = F / M
-Mover.prototype.applyForce = function(force) {
-  var f = p5.Vector.div(force,this.mass);
-  this.acceleration.add(f);
-};
-Mover.prototype.checkRebote = function() {
-  return this.cotadorRebotes !== this.maxRebotes;
-};
-
-Mover.prototype.updateDown = function() {
-  // La velocidad es cambiada según la aceleración
-  this.velocity.add(this.acceleration);
-  // La posición es cambiada según la velocidad
-  this.position.add(this.velocity);
-  // Borrar aceleración en cada cuadro
-  this.acceleration.mult(0);
-};
-
-Mover.prototype.updateUp = function() {
-
-};
-
-Mover.prototype.updateDown = function() {
-  // La velocidad es cambiada según la aceleración
-  this.velocity.add(this.acceleration);
-  // La posición es cambiada según la velocidad
-  this.position.add(this.velocity);
-  // Borrar aceleración en cada cuadro
-  this.acceleration.mult(0);
-};
-
-Mover.prototype.display = function() {
-  /*stroke(0);
-  strokeWeight(2);
-  fill(255,127);*/
-  image(logo, this.position.x,this.position.y,this.mass*16,this.mass*16);
-  //ellipse(this.position.x,this.position.y,this.mass*16,this.mass*16);
-};
+  };
 
 // Rebotar contra la parte inferior de la ventana
-Mover.prototype.checkEdges = function() {
+  this.checkEdges = function() {
 
-  if (this.position.y > ((height - (height/10)) - this.mass*8)) {
-    // Un poco de amortiguamiento al rebotar contra el fondo
-    this.velocity.y *= -0.9;
-    this.position.y = ((height - (height / 10)) - this.mass * 8);
-    this.cotadorRebotes += 1; console.log(this.cotadorRebotes );
-  }
+    if (this.position.y > ((height - (height/10)) - this.mass*8)) {
+      // Un poco de amortiguamiento al rebotar contra el fondo
+      this.velocity.y *= -0.9;
+      this.position.y = ((height - (height / 10)) - this.mass * 8);
+      this.cotadorRebotes += 1;
+      // console.log(this.cotadorRebotes );
+    }
 
-};
+  };
+}
 
-// snowflake class
-function snowflake() {
+// Snowflake class
+function Snowflake() {
   // initialize coordinates
   this.posX = 0;
   this.posY = random(-50, 0);
   this.initialangle = random(0, 2 * PI);
   this.size = random(2, 5);
 
-  // radius of snowflake spiral
-  // chosen so the snowflakes are uniformly spread out in area
+  // radius of Snowflake spiral
+  // chosen so the Snowflakes are uniformly spread out in area
   this.radius = sqrt(random(pow(width / 2, 2)));
 
   this.update = function(time) {
@@ -247,16 +305,15 @@ function snowflake() {
     let angle = w * time + this.initialangle;
     this.posX = width / 2 + this.radius * sin(angle);
 
-    // different size snowflakes fall at slightly different y speeds
+    // different size Snowflakes fall at slightly different y speeds
     this.posY += pow(this.size, 0.5);
 
-    // delete snowflake if past end of screen
+    // delete Snowflake if past end of screen
     if (this.posY > (height - (height/3) + 5)) {
       let index = snowflakes.indexOf(this);
       snowflakes.splice(index, 1);
     }
   };
-
   this.display = function() {
     ellipse(this.posX, this.posY, this.size);
   };
